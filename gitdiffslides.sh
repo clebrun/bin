104340d0e884204b19d5e1a306d4d3a96855919b
Cooper LeBrun <cooperlebrun@gmail.com>
2023-11-18 13:55:17 -0500

    Cleaned out unused / obsolete scripts

--- gitdiffslides.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# don't use -n for git log if no arguments given
-# check git show args to cut down on visual noise?
-git log --oneline -n $1 | grep -v Merge | cut -d' ' -f1 | tac | xargs git show
